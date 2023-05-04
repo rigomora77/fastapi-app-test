fac37f8aa75889cdee7690282879776baeeb1be2
rigomora77 <rmora@dilarce.com>
2023-05-04 17:30:45 -0600

    Pruebas de cambios realizados

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(title='Prueba de aplicación de FastAPI',
+              description='Prueba automatizada de actualización del app en google Run, utilizando DockerFile')
 
 @app.get('/')
 async def root():
-    return {'mensaje':'Hola desde FastAPI'}
+    return {'mensaje':'Hola desde FastAPI Test02'}
